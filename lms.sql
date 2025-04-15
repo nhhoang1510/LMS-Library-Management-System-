eb79c5ab77ccf01b2dd9bce1e8428fc700494c0a
nhhoang1510 <nevada15102004@gmail.com>
2025-04-15 11:50:57 +0700

    lms.sql

--- lms.sql
+++ lms.sql
@@ -1,32 +1,7 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: May 08, 2020 at 10:42 AM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.2.26
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `lms`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `admins`
---
 
 CREATE TABLE `admins` (
   `id` int(11) NOT NULL,
